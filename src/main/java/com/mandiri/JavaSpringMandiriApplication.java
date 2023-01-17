04c7e16c9063265958a1317d8920ed2b8f2b2da2
farhanmozart <fmfachreza@gmail.com>
2023-01-17 10:44:03 +0700

    [FEATURE] adding login controller and JSP

---
+++ src/main/java/com/mandiri/JavaSpringMandiriApplication.java
@@ -0,0 +1,13 @@
+package com.mandiri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringMandiriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringMandiriApplication.class, args);
+	}
+
+}
---
+++ src/main/resources/META-INF/resources/WEB-INF/jsp/sayHello.jsp
@@ -0,0 +1,8 @@
+<html>	
+	<head>
+		<title> My First HTML Page - JSP </title>
+	</head>
+	<body>
+		Hallo Farhan Mozart Aditya Fahreza!
+	</body>
+</html>
